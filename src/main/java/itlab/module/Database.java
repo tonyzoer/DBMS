db2d22475951931b08662bd87ae4a7367040b0c1
blowdee <blowdee1@gmail.com>
2017-10-10 21:41:05 +0300

    Proj reload LUL

---
+++ src/main/java/itlab/module/Database.java
@@ -0,0 +1,136 @@
+package itlab.module;
+
+import itlab.module.exceptions.TableAlreadyExsists;
+import itlab.module.exceptions.NonExistingTable;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Database implements Serializable {
+    Map<String, Table> tables;
+    String name;
+
+    public Database(String name, Map<String, Table> tables) {
+        this.tables = tables;
+        this.name = name;
+    }
+
+    public Database(String name) {
+        this.name = name;
+        tables = new HashMap<>();
+    }
+
+    public Map<String, Table> getTables() {
+        return tables;
+    }
+
+    public String createTable(String name, Scheme scheme) throws TableAlreadyExsists {
+        if (!tables.containsKey(name)) {
+            Table t = new Table(name, scheme);
+            tables.put(name, t);
+            return name;
+        } else {
+            throw new TableAlreadyExsists(name + " table already exists");
+        }
+    }
+
+    public void deleteTable(String name) {
+        tables.remove(name);
+    }
+
+    public Table getTable(String name) throws NonExistingTable {
+
+        if (tables.containsKey(name))
+            return tables.get(name);
+        else throw new NonExistingTable("Table " + name +" in database " + this.name + " not exists");
+    }
+
+    public void save() {
+        try {
+            String PATH = "/remote/dir/server/";
+            String directoryName = PATH.concat("database/");
+            String filename = name + ".db";
+
+            File directory = new File(directoryName);
+            if (!directory.exists()) {
+                directory.mkdirs();
+                // If you require it to make the entire directory path including parents,
+                // use directory.mkdirs(); here instead.
+            }
+
+            File f = new File(directoryName + filename);
+            FileOutputStream fos = new FileOutputStream(f);
+            ObjectOutputStream out = new ObjectOutputStream(fos);
+            out.writeObject(this);
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void load() {
+        try {
+            String PATH = "/remote/dir/server/";
+            String directoryName = PATH.concat("database/");
+            String filename = name + ".db";
+
+            File directory = new File(directoryName);
+            if (!directory.exists()) {
+                directory.mkdirs();
+                // If you require it to make the entire directory path including parents,
+                // use directory.mkdirs(); here instead.
+            }
+            Object db = new ObjectInputStream(new FileInputStream(directoryName + filename)).readObject();
+            if (db instanceof Database) {
+                Database lDatabase = (Database) db;
+                this.tables = lDatabase.tables;
+            }
+        } catch (ClassNotFoundException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void delete() {
+        String PATH = "/remote/dir/server/";
+        String directoryName = PATH.concat("database/");
+        String filename = name + ".db";
+
+        File directory = new File(directoryName);
+        if (!directory.exists()) {
+            directory.mkdirs();
+            // If you require it to make the entire directory path including parents,
+            // use directory.mkdirs(); here instead.
+        }
+        File f = new File(directoryName + filename);
+        f.delete();
+    }
+
+    public List<Row> substractTables(String table1, String table2) throws NonExistingTable {
+        if(tables.get(table1) == null || tables.get(table2) == null) throw new NonExistingTable();
+        Table first = tables.get(table1);
+        Table second = tables.get(table2);
+        if(!first.getScheme().equals(second.getScheme())) return null;
+
+        ArrayList<Row> result = new ArrayList<>();
+        for(Map.Entry<String, Row> item : first.getRows().entrySet()){
+            if(!item.getValue().equals(second.getRow(item.getKey()))) result.add(item.getValue());
+        }
+        for(Map.Entry<String, Row> item : second.getRows().entrySet()){
+            if(!item.getValue().equals(first.getRow(item.getKey()))) result.add(item.getValue());
+        }
+
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Database{" +
+                "tables=" + tables +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
